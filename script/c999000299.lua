d3dc0df8191f43dcf362efe80f04097bdfcd0513
Ferroluchano <Luchano_perro@hotmail.com>
2023-06-07 11:10:12 -0300

    Update c999000299.lua

--- script/c999000299.lua
+++ script/c999000299.lua
@@ -4,13 +4,11 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCountLimit(1)
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTarget(s.tg)
-	e1:SetOperation(s.op)
+    e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x35}
@@ -39,5 +37,10 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+	function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsPosition(POS_FACEUP_ATTACK) then
+		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)
 	end
 end
