1a79382c6c45e00df1712a621db5e8c12ad1b3f2
Ferroluchano <Luchano_perro@hotmail.com>
2023-06-07 11:06:17 -0300

    Update c999000299.lua

--- script/c999000299.lua
+++ script/c999000299.lua
@@ -2,33 +2,29 @@
 --Fabled Oltro
 local s,id=GetID()
 function s.initial_effect(c)
-	--Cambiar pocision
+	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_POSITION)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTarget(s.tg)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x35}
-function s.filter(c)
-	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition()
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x35) and c:IsMonster() and c:GetLevel()==3 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return false end
-		local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
-		local res=nil
-		if #g==0 then
-			res=false
-		elseif #g==1 then
-			res=Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,g:GetFirst())
-		else res=Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
-		return res
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 	end
-	local g=Duel.GetMatchingGroup(s.,tp,LOCspfilterATION_HAND,0,nil,e,tp)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
 	local cg=nil
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	if #g==1 then
