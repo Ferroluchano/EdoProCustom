7a2f41af09c111bfd7745b86ea32a111cd43c515
Ferroluchano <Luchano_perro@hotmail.com>
2023-06-17 08:28:39 -0300

    Update c999000299.lua

--- script/c999000299.lua
+++ script/c999000299.lua
@@ -22,24 +22,24 @@ end
 
 -- Paga el costo de enviar una carta de la mano al Cementerio
 function c999000299.cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost, tp, LOCATION_HAND, 0, 1, nil) end
+    if chk == 0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost, tp, LOCATION_HAND, 0, 1, e:GetHandler()) end
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TOGRAVE)
-    local g = Duel.SelectMatchingCard(tp, Card.IsAbleToGraveAsCost, tp, LOCATION_HAND, 0, 1, 1, nil)
+    local g = Duel.SelectMatchingCard(tp, Card.IsAbleToGraveAsCost, tp, LOCATION_HAND, 0, 1, 1, e:GetHandler())
     Duel.SendtoGrave(g, REASON_COST)
 end
 
 -- Elige y cambia de posición a un monstruo enemigo
 function c999000299.target(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return Duel.IsExistingTarget(Card.IsFaceup, 1 - tp, LOCATION_MZONE, 0, 1, nil) end
+    if chk == 0 then return Duel.IsExistingTarget(Card.IsFaceup, 1 - tp, 0, LOCATION_MZONE, 1, nil) end
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_POSCHANGE)
-    local g = Duel.SelectTarget(tp, Card.IsFaceup, 1 - tp, LOCATION_MZONE, 1, 1, nil)
+    local g = Duel.SelectTarget(tp, Card.IsFaceup, 1 - tp, 0, 1, 1, nil)
     Duel.SetOperationInfo(0, CATEGORY_POSITION, g, 1, 0, 0)
 end
 
 -- Cambia la posición del monstruo seleccionado
 function c999000299.operation(e, tp, eg, ep, ev, re, r, rp)
     local tc = Duel.GetFirstTarget()
-    if tc:IsRelateToEffect(e) and tc:IsAttackPos() then
+    if tc and tc:IsRelateToEffect(e) and tc:IsAttackPos() then
         Duel.ChangePosition(tc, POS_FACEUP_DEFENSE)
     end
 end
